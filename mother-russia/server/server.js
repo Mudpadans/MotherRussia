754139a0569a4380dc9a99f69fc1cccda4e6ac3c
Mudpadans <philmodel.101@gmail.com>
2024-01-31 18:03:23 -0500

    new beginnings

---
+++ mother-russia/server/server.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+const PORT = process.env.PORT
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(PORT, function(error) {
+    if (error) {
+        console.log('Something went wrong', error)
+    } else {
+        console.log('Server is listening on port ' + PORT)
+    }
+})
