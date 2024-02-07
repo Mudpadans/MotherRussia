fc29532fa837804442739aaeaaf516807db9b879
Mudpadans <philmodel.101@gmail.com>
2024-02-07 16:40:06 -0500

    translator-api

--- mother-russia/src/App.js
+++ mother-russia/src/App.js
@@ -5,19 +5,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>MotherRussia.com</h2>
+        <h2>Word Lists</h2>
+        <h2>Grammar</h2>
+        <h2>Pronunciation</h2>
       </header>
+      <section id="translator">
+
+      </section>
     </div>
   );
 }
