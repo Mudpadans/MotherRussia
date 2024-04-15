7f3c1fe5dfeae8529b11eb0b4d556dd904c2e2cd
Mudpadans <philmodel.101@gmail.com>
2024-04-15 15:36:15 -0400

    new words

--- mother-russia/src/App.js
+++ mother-russia/src/App.js
@@ -2,6 +2,7 @@
 import React from 'react'
 import './App.css';
 import Translator from './Components/Translator';
+import WordLists from './Components/WordLists';
 // import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
 
@@ -16,13 +17,14 @@ function App() {
           <h2>MotherRussia.com</h2>
           <nav>
             <Link to="/"><h2>Home</h2></Link>
-            <Link><h2>WordLists</h2></Link>
+            <Link to="/Wordlists"><h2>WordLists</h2></Link>
             <Link><h2>Grammar</h2></Link>
             <Link><h2>Pronunciation</h2></Link>
           </nav>
         </header>
         <Routes>
           <Route path="/" element={<Translator />}/>
+          <Route path="/Wordlists" element={<WordLists />}/>
         </Routes>
             
       </div>
---
+++ mother-russia/src/Components/WordLists.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import axios from 'axios';
+import './WordLists.css'
+
+function WordLists() {
+
+    return (
+        <div>
+            <h1>Wordlists</h1>
+                <h2>Greetings & Expressions</h2>
+                    <ul>
+                        <li>Hello - Здравствуйте (Zdravstvuytye) [formal]; Привет (Privyet) [informal]</li>
+                        <li>Goodbye - До свидания (Do svidaniya) [formal]; Пока (Poka) [informal]</li>
+                        <li>Yes - Да (Da)</li>
+                        <li>No - Нет (Nyet)</li>
+                        <li>Please - Пожалуйста (Pozhaluysta)</li>
+                        <li>Thank you - Спасибо (Spasibo)</li>
+                        <li>Sorry - Извините (Izvinite) [formal]; Прости (Prosti) [informal]</li>
+                        <li>Excuse me - Извините (Izvinite) [to get someone's attention or to apologize]</li>
+                        <li>How are you? - Как дела? (Kak dela?) [informal]; Как Вы? (Kak Vy?) [formal]</li>
+                        <li>What's your name? - Как тебя зовут? (Kak tebya zovut?) [informal]; Как Вас зовут? (Kak Vas zovut?) [formal]</li>
+                        <li>Nice to meet you - Приятно познакомиться (Priyatno poznakomit'sya)</li>
+                        <li>I don't understand - Я не понимаю (Ya ne ponimayu)</li>
+                        <li>Could you repeat that? - Не могли бы вы повторить? (Ne mogli by vy povtorit?) [formal]; Можешь повторить? (Mozhesh' povtorit?) [informal]</li>
+                        <li>Where is the bathroom? - Где туалет? (Gde tualet?)</li>
+                    </ul>
+                <h2>Numbers 1-100</h2>
+                    <ul>
+                        <li>1 - Один (Odin)</li>
+                        <li>2 - Два (Dva)</li>
+                        <li>3 - Три (Tri)</li>
+                        <li>4 - Четыре (Chetyre)</li>
+                        <li>5 - Пять (Pyat')</li>
+                        <li>6 - Шесть (Shest')</li>
+                        <li>7 - Семь (Sem')</li>
+                        <li>8 - Восемь (Vosem')</li>
+                        <li>9 - Девять (Devyat')</li>
+                        <li>10 - Десять (Desyat')</li>
+                        <li>11 - Одиннадцать (Odinnadtsat')</li>
+                        <li>12 - Двенадцать (Dvenadtsat')</li>
+                        <li>13 - Тринадцать (Trinadtsat')</li>
+                        <li>14 - Четырнадцать (Chetyrnadtsat')</li>
+                        <li>15 - Пятнадцать (Pyatnadtsat')</li>
+                        <li>16 - Шестнадцать (Shestnadtsat')</li>
+                        <li>17 - Семнадцать (Semnadtsat')</li>
+                        <li>18 - Восемнадцать (Vosemnadtsat')</li>
+                        <li>19 - Девятнадцать (Devyatnadtsat')</li>
+                        <li>20 - Двадцать (Dvadtsat')</li>
+                        <li>21 - Двадцать один (Dvadtsat' odin)</li>
+                        <li>22 - Двадцать два (Dvadtsat' dva)</li>
+                        <li>23 - Двадцать три (Dvadtsat' tri)</li>
+                        <li>24 - Двадцать четыре (Dvadtsat' chetyre)</li>
+                        <li>25 - Двадцать пять (Dvadtsat' pyat')</li>
+                        <li>26 - Двадцать шесть (Dvadtsat' shest')</li>
+                        <li>27 - Двадцать семь (Dvadtsat' sem')</li>
+                        <li>28 - Двадцать восемь (Dvadtsat' vosem')</li>
+                        <li>29 - Двадцать девять (Dvadtsat' devyat')</li>
+                        <li>30 - Тридцать (Tridtsat')</li>
+                        <li>40 - Сорок (Sorok)</li>
+                        <li>50 - Пятьдесят (Pyat'desyat)</li>
+                        <li>60 - Шестьдесят (Shest'desyat)</li>
+                        <li>70 - Семьдесят (Sem'desyat)</li>
+                        <li>80 - Восемьдесят (Vosem'desyat)</li>
+                        <li>90 - Девяносто (Devyanosto)</li>
+                        <li>100 - Сто (Sto)</li>
+                    </ul>
+                <h2>Days of the Week</h2> 
+                    <ul>
+                        <li>Monday - Понедельник (Ponedel'nik)</li>
+                    </ul>
+        </div>
+    )
+}
+
+export default WordLists
