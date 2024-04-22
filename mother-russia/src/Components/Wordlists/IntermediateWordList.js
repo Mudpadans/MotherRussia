58789a61172000dfb94499b3aaf546e0198dd1f0
Mudpadans <philmodel.101@gmail.com>
2024-04-22 13:57:51 -0400

    oh look, more words

--- mother-russia/src/Components/Wordlists/IntermediateWordList.js
+++ mother-russia/src/Components/Wordlists/IntermediateWordList.js
@@ -23,6 +23,130 @@ function IntermediateWordList() {
                         <li>Delayed - Задержан (Zaderzhan)</li>
                         <li>Cancelled - Отменен (Otmenen)</li>
                     </ul>
+                <h2>Common Phrases for Buying Tickets</h2>
+                    <ul>
+                        <li>One ticket to Moscow, please. - Один билет до Москвы, пожалуйста. (Odin bilet do Moskvy, pozhaluysta.)</li>
+                        <li>Where can I buy a ticket? - Где я могу купить билет? (Gde ya mogu kupit' bilet?)</li>
+                        <li>Is this seat available? - Это место свободно? (Eto mesto svobodno?)</li>
+                        <li>I’d like to book a seat by the window. - Я бы хотел забронировать место у окна. (Ya by khotel zabronirovat' mesto u okna.)</li>
+                    </ul>
+                <h2>Vocabulary for Asking for Directions</h2>
+                    <ul>
+                        <li>Where is...? - Где находится...? (Gde nakhoditsya...?)</li>
+                        <li>How do I get to...? - Как мне добраться до...? (Kak mne dobratsya do...?)</li>
+                        <li>Is it far? - Это далеко? (Eto daleko?)</li>
+                        <li>To the left. - Налево. (Nalevo.)</li>
+                        <li>To the right. - Направо. (Napravo.)</li>
+                        <li>Straight ahead. - Прямо. (Pryamo.)</li>
+                        <li>Map - Карта (Karta)</li>
+                    </ul>
+                <h2>Common Phrases Used During Travel</h2>
+                    <ul>
+                        <li>Can you show me on the map? - Можете показать на карте? (Mozhete pokazat' na karte?)</li>
+                        <li>I need a taxi. - Мне нужно такси. (Mne nuzhno taksi.)</li>
+                        <li>How much does it cost? - Сколько это стоит? (Skol'ko eto stoit?)</li>
+                        <li>Please stop here. - Остановитесь здесь, пожалуйста. (Ostanovites' zdes', pozhaluysta.)</li>
+                        <li>I am lost. - Я потерялся (if male)/потерялась (if female). (Ya poteryalsya/poteryalas'.)</li>
+                        <li>Does this train/bus go to...? - Этот поезд/автобус идет до...? (Etot poyezd/avtobus idet do...?)</li>
+                    </ul>
+                <h2>Types of Stores</h2>
+                    <ul>
+                        <li>Supermarket - Супермаркет (Supermarket)</li>
+                        <li>Department Store - Универмаг (Universam)</li>
+                        <li>Pharmacy - Аптека (Apteka)</li>
+                        <li>Bookstore - Книжный магазин (Knizhnyy magazin)</li>
+                        <li>Clothing Store - Магазин одежды (Magazin odezhdy)</li>
+                        <li>Shoe Store - Магазин обуви (Magazin obuvi)</li>
+                        <li>Grocery Store - Продуктовый магазин (Produktovyy magazin)</li>
+                        <li>Market - Рынок (Rynok)</li>
+                        <li>Bakery - Булочная (Bulochnaya)</li>
+                        <li>Butcher Shop - Мясная лавка (Myasnaya lavka)</li>
+                        <li>Fish Market - Рыбный рынок (Rybn'y rynok)</li>
+                        <li>Electronics Store - Магазин электроники (Magazin elektroniki)</li>
+                    </ul>
+                <h2>Shopping Vocabulary</h2>
+                    <ul>
+                        <li>Price - Цена (Tsena)</li>
+                        <li>Discount - Скидка (Skidka)</li>
+                        <li>Sale - Распродажа (Rasprodazha)</li>
+                        <li>Cashier - Кассир (Kassir)</li>
+                        <li>Receipt - Чек (Chek)</li>
+                        <li>Shopping Cart - Тележка (Telezhka)</li>
+                        <li>Shopping Bag - Пакет (Paket)</li>
+                        <li>Fitting Room - Примерочная (Primerochnaya)</li>
+                        <li>Size - Размер (Razmer)</li>
+                        <li>Exchange - Обмен (Obmen)</li>
+                        <li>Return - Возврат (Vozvrat)</li>
+                    </ul>
+                <h2>Phrases for Bargaining and Discussing Prices</h2>
+                    <ul>
+                        <li>How much is this? - Сколько это стоит? (Skol'ko eto stoit?)</li>
+                        <li>Can you give a better price? - Можете сделать цену ниже? (Mozhete sdelat' tsenu nizhe?)</li>
+                        <li>It's too expensive. - Это слишком дорого. (Eto slishkom dorogo.)</li>
+                        <li>Do you have anything cheaper? - У вас есть что-нибудь подешевле? (U vas est' chto-nibud' podeshevle?)</li>
+                        <li>I'm just looking, thanks. - Я просто смотрю, спасибо. (Ya prosto smotryu, spasibo.)</li>
+                        <li>I would like to buy this. - Я бы хотел купить это. (Ya by khotel kupit' eto.)</li>
+                        <li>Can I pay by card? - Можно платить картой? (Mozhno platit' kartoy?)</li>
+                        <li>Is there a discount for cash? - Есть ли скидка за наличные? (Est' li skidka za nalichnye?)</li>
+                    </ul>
+                <h2>Asking for Help</h2>
+                    <ul>
+                        <li>Where can I find...? - Где я могу найти...? (Gde ya mogu nayti...?)</li>
+                        <li>Do you have this in another color? - У вас есть это в другом цвете? (U vas est' eto v drugom tsvete?)</li>
+                        <li>I need help. - Мне нужна помощь. (Mne nuzhna pomoshch')</li>
+                    </ul>
+                <h2>Medical Terms</h2>
+                    <ul>
+                        <li>Doctor - Доктор (Doktor)</li>
+                        <li>Nurse - Медсестра (Medsestra)</li>
+                        <li>Hospital - Больница (Bol'nitsa)</li>
+                        <li>Clinic - Клиника (Klinika)</li>
+                        <li>Pharmacy - Аптека (Apteka)</li>
+                        <li>Medicine - Лекарство (Lekarstvo)</li>
+                        <li>Prescription - Рецепт (Retsept)</li>
+                        <li>Surgery - Хирургия (Khirurgiya)</li>
+                        <li>Vaccine - Вакцина (Vaktsina)</li>
+                        <li>Emergency Room (ER) - Приемное отделение (Priemnoe otdelenie)</li>
+                    </ul>
+                <h2>Symptoms</h2>
+                    <ul>
+                        <li>Fever - Жар (Zhar)</li>
+                        <li>Cough - Кашель (Kashel')</li>
+                        <li>Headache - Головная боль (Golovnaya bol')</li>
+                        <li>Stomachache - Боль в животе (Bol' v zhivote)</li>
+                        <li>Nausea - Тошнота (Toshnota)</li>
+                        <li>Dizziness - Головокружение (Golovokruzhenie)</li>
+                        <li>Pain - Боль (Bol')</li>
+                        <li>Injury - Травма (Travma)</li>
+                        <li>Allergy - Аллергия (Allergiya)</li>
+                        <li>Cold - Простуда (Prostuda)</li>
+                    </ul>
+                <h2>Pharmacy Essentials</h2>
+                    <ul>
+                        <li>Painkiller - Обезболивающее (Obezbolivayushchee)</li>
+                        <li>Antibiotic - Антибиотик (Antibiotik)</li>
+                        <li>Bandage - Бинт (Bint)</li>
+                        <li>Antiseptic - Антисептик (Antiseptik)</li>
+                        <li>Thermometer - Термометр (Termometr)</li>
+                        <li>Cough Syrup - Сироп от кашля (Sirop ot kashlya)</li>
+                        <li>Vitamins - Витамины (Vitaminy)</li>
+                    </ul>
+                <h2>Discussing Emergencies</h2>
+                    <ul>
+                        <li>I need a doctor. - Мне нужен доктор. (Mne nuzhen doktor.)</li>
+                        <li>Call an ambulance! - Вызовите скорую помощь! (Vyzyvite skoruyu pomosh'!)</li>
+                        <li>It's an emergency. - Это чрезвычайная ситуация. (Eto chrezvychaynaya situatsiya.)</li>
+                        <li>I'm allergic to... - У меня аллергия на... (U menya allergiya na...)</li>
+                        <li>I feel sick. - Мне плохо. (Mne ploho.)</li>
+                    </ul>
+                <h2>Talking to Doctors</h2>
+                    <ul>
+                        <li>What’s wrong with me? - Что со мной? (Chto so mnoy?)</li>
+                        <li>It hurts here. - Здесь болит. (Zdes' bolit.)</li>
+                        <li>I have been feeling... - Я чувствую... (Ya chuvstvuyu...)</li>
+                        <li>How long should I take this medicine? - Как долго мне принимать это лекарство? (Kak dolgo mne prinimat' eto lekarstvo?)</li>
+                        <li>Are there any side effects? - Есть ли побочные эффекты? (Est' li pobochnye effekty?)</li>
+                    </ul>
         </div>
     )
 }
